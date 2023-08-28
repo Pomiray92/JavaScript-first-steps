6c02085ae41b5815f26cab06f41d4f13b65c1df1
Pomiray92 <soma.gulov@gmail.com>
2023-08-28 20:50:38 +0200

    feature/ Getter-Setter, Enumirate Properties, Value Referense Types + an Exercise

--- README.md
+++ README.md
@@ -45,3 +45,77 @@ console.log(person1.constructor); // Outputs: ƒ Person(name) { this.name = name
 ```
 
 
+## Value vs Reference Types
+
+### Value Types (Primitive Types)
+Value types, also known as primitive types, include data that is stored directly in memory. When you work with value types, you're dealing with the actual value itself. Examples of value types include:
+
+- Number
+- String
+- Boolean
+- Symbol
+- undefined
+- null
+
+With value types, when you assign or copy a variable, you're making a distinct copy of the value. Changing one variable does not affect the others.
+
+Example 1:
+```javascript
+let x = 10;
+let y = x;
+
+x = 20;
+
+console.log("y:", y); // Output: y: 10
+console.log("x:", x); // Output: x: 20
+```
+
+Example 2:
+```javascript
+let number = 10;
+function increase(number) {
+    number++;
+}
+
+increase(number);
+console.log("Primitive Type:", number); // Output: Primitive Type: 10
+```
+
+
+### Reference Types (Objects)
+Reference types encompass data that is stored as a reference or address in memory. When you work with reference types, you're working with a reference to the location of the actual data. Examples of reference types include:
+
+- Object
+- Function
+- Array
+
+When you assign or copy a reference type variable, you're copying a reference to the underlying data. If you modify the data through one variable, it will affect all variables that reference the same data.
+
+Example:
+```javascript
+let x1 = { value: 10 };
+let y1 = x1;
+
+x1.value = 20;
+
+console.log("y1:", y1); // Output: y1: { value: 20 }
+console.log("x1:", x1); // Output: x1: { value: 20 }
+```
+Example 2:
+```javascript
+let number1 = {value: 10};
+
+function increase(number1) {
+    number1.value++;
+}
+
+increase(number1);
+console.log("Reference Type:", number1); // Output: Reference Type: { value: 11 }
+```
+
+
+#### TAKEAWAY: 
+  - `PREMERIVES` are copied by they value
+  - `Objects` are copird by their reference
+
+Understanding the distinction between value and reference types is essential for effectively managing and manipulating data in JavaScript.
--- objects.js
+++ objects.js
@@ -40,23 +40,225 @@
 // // ################### Construction Function ################ //
 
 
-function Circle(radius) {
-    console.log('this', this);
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
+// function Circle(radius) {
+//     console.log('this', this);
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
 
-const Circle1 = new Function("radius", `this.radius = radius;
-this.draw = function() {
-    console.log('draw');
-}
-`);
+// const Circle1 = new Function("radius", `this.radius = radius;
+// this.draw = function() {
+//     console.log('draw');
+// }
+// `);
 
-const circle = new Circle1(1);
+// // Using the constructor function to create an instance
+// const circle = new Circle(1);
 
-const another = new Circle(1); // "new" operator
+// // Using the `call` method to invoke the Circle function with a custom `this` value
+// Circle.call({}, 1); // The first argument is the custom `this` value, and subsequent arguments are function arguments
+
+// // Using the `apply` method to invoke the Circle function with a custom `this` value and an array of function arguments
+// Circle.apply({}, [1, 2, 3]);
 
 
 // Functions are Objects? 
+
+// ################### Value vs Reference Types ################ //
+
+// // Value Types
+// let x = 10;
+// let y = x;
+
+// console.log("y:", y); // Output: y: 10
+// console.log("x:", x); // Output: x: 20
+// // Primitives are copied by they value
+
+
+// // Reference Types
+// let x1 = {value: 10};
+// let y1 = x1;
+
+// console.log("y1:", y1); // Output: y1: { value: 20 }
+// console.log("x1:", x1); // Output: x1: { value: 20 }
+// // Objects are copied by their reference
+
+// let number = 10;
+// function increase(number) {
+//     number++;
+// }
+
+// increase(number);
+// console.log("Primitive Type:", number);
+
+
+
+// let number1 = {value: 10};
+
+// function increase(number1) {
+//     number1.value++;
+// }
+
+// increase(number1);
+// console.log("Reference Type:", number1);
+
+// function Circle(radius) {
+//     console.log('this', this);
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const circle = new Circle(10);
+
+// // user.token = 'asdadsadsad';
+
+// circle.location = {x: 1};
+
+// const propertyName = 'center-location';
+// circle[propertyName] = {x: 1};
+
+// delete circle[propertyName]; // or delete circle['location']
+// delete circle.drow
+
+// ################### Enumerating Properties ################ //
+// consept of closuare
+// function Circle(radius) {
+//     // console.log('this', this);
+
+//     // let color = 'red'; // local func variable
+
+
+//     this.radius = radius;
+//     let defoultLocation = { x: 0, y: 0};
+//     let computeOptimumLocation = function(factor) {
+//         // ...
+//     }
+
+//     this.draw = function() {
+//         // let x, y;                       // scopes are temprolely
+//         computeOptimumLocation(0.1);        // call comput... method inside drow method
+//         console.log('draw');
+//     };
+// } 
+
+// const circle = new Circle(10);
+// circle.draw();
+// // console.log('test:', circle.draw)
+// // circle.drow.computeOptimumLocation();
+// // circle.location = {x: 1};
+// // const propertyName = 'center-location';
+// // circle[propertyName] = {x: 1};
+
+// for (let key in circle) {
+//     if (typeof circle[key] !== 'function')
+//         console.log(key, circle[key]);
+// }
+
+// const keys = Object.keys(circle);
+// console.log(keys);
+
+// if ('radius' in circle)
+//     console.log('Circle has a radius of', circle.radius)
+
+// ################### Getters/Setters ################ //
+// ########## First get method 
+
+// function Circle(radius) {
+//     this.radius = radius;
+
+//     let defoultLocation = { x: 0, y: 0};    
+//     let computeOptimumLocation = function(factor) {
+//         // ...
+//     }
+
+//     this.getDefoultLocation = function() {
+//         return defoultLocation;
+//     };
+
+//     this.draw = function() {       
+//         console.log('draw');
+//     };
+// }
+
+// const circle = new Circle(10);
+// circle.getDefoultLocation();
+// circle.draw();
+
+
+// ######### Second get method 
+// function Circle(radius) {
+//     this.radius = radius;
+
+//     let defoultLocation = { x: 0, y: 0};    
+//     // let computeOptimumLocation = function(factor) {
+//     //     // ...
+//     // }
+
+//     this.getDefoultLocation = function() {
+//         return defoultLocation;
+//     };
+
+//     this.draw = function() {       
+//         console.log('draw');
+//     };
+
+//     Object.defineProperty(this, 'defoultLocation', {
+//         get: function() {
+//             return defoultLocation
+//         },
+//         set: function(value) {
+//             if (!value.x || !value.y)
+//                 throw new Error('invalid Location.');
+//             defoultLocation = value;
+//         }
+//     });
+// }
+
+
+// const circle = new Circle(10);
+// circle.defoultLocation = 1;
+// //circle.defoultLocation
+// circle.draw();
+
+// ################### Exercise ################ //
+
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function() {
+        if (running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true
+
+        startTime = new Date();
+    };
+
+    this.stop = function() {
+        if (!running)
+            throw new Error('Stopwatch is not started.');
+
+        running = false;
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+
+        duration += seconds;
+    };
+        
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() { return duration; }
+    });
+}
